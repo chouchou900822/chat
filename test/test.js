88921e2fad4c013323df7aa50e5d6029425fabc9
edison <edison900822@gmail.com>
2017-11-02 18:08:50 +0800

    测试

--- test/test.js
+++ test/test.js
@@ -1,8 +1,29 @@
 'use strict';
-require('should');
+var should = require('should');
+function factorial(n) {
+  return n > 1 ? n * factorial(n-1) : 1;
+}
+describe("year test", function () {
+  it("1的阶乘是1", function () {
+    factorial(1).should.be.exactly(1).and.be.a.Number;
+  });
+  it("2的阶乘是2", function () {
+    factorial(2).should.be.exactly(2).and.be.a.Number;
+  });
+  it("3的阶乘是6", function () {
+    factorial(3).should.be.exactly(6).and.be.a.Number;
+  });
+});
 
-describe("year test", function() {
-  it("结果是对的", function() {
-    ('123').should.match(/123/)
+var fs = require("fs");
+
+describe("readFile", function() {
+  it("读取的文本应该是hello world!", function(done) {
+    fs.readFile("./test/text.txt", "utf8", function(err, data) {
+      should.not.exist(err);
+      should.exist(data);
+      data.should.eql("hello world!");
+      done();
+    });
   });
 });
